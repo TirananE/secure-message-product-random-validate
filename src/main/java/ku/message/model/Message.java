1496edbd583675ba016fcd3f6854080c9e0e0453
Tiranan <tiranan.e@ku.th>
2021-05-08 16:27:08 +0700

    Finish Password Validation

---
+++ src/main/java/ku/message/model/Message.java
@@ -0,0 +1,20 @@
+package ku.message.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String user;
+    private String text;
+    private Instant createdAt;
+}
